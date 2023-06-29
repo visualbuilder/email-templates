3fc6253639e29afc744e89ba9b2a387673c919a2
Lee Evans <lee@ekouk.com>
2023-06-29 17:07:39 +0100

    Added Token helper function to replace any instance of ##model.attribute## or ##config.xxxx## for config values.
    Added Allowed config token list -> eg config.app.name. - We shouldn't allow all config values to be inserted as they may contain secret information.

--- config/email-templates.php
+++ config/email-templates.php
@@ -4,6 +4,7 @@ return [
     'default-locale'=>'en_GB',
     'default-view'=>'preview-email-template',
     'header-colour' => '#4a2725',
+    
     //Models who can receive emails
     'recipients'    => [
         (object)[
@@ -11,8 +12,22 @@ return [
             'name'  => 'User',
             'model' => '\\App\\Models\\User'],
     ],
-    //Guards who are authorised to edit templates
+    
+    //Guards who are authorised to edit email templates
     'editor-guards'=>['web'],
+    
+    /**
+     * Allowed config keys which can be inserted into email templates
+     * eg use ##config.app.name## in the email template for automatic replacement.
+     */
+    'config-keys' => [
+        'app.name',
+        'app.url',
+        // Add other safe config keys here.
+        // We don't want to allow all config keys they may contain secret keys or credentials
+    ],
+    
+    
     // Form Field Labels
     'field-labels' => [
         'template-name' => 'Template Display Name',
--- src/Helpers/TokenHelper.php
+++ src/Helpers/TokenHelper.php
@@ -6,8 +6,50 @@ use Visualbuilder\EmailTemplates\Contracts\TokenHelperInterface;
 
 class TokenHelper implements TokenHelperInterface
 {
-    public function replaceTokens($string, $model)
+    public function replaceTokens($content, $model)
     {
-        return $string;
+        // Replace singular tokens.
+        // These are for password reset and email verification
+        if (isset($models->tokenUrl)) {
+            $content = str_replace('##tokenURL##', $models->tokenUrl, $content);
+        }
+        
+        if (isset($models->verificationUrl)) {
+            $content = str_replace('##verificationUrl##', $models->verificationUrl, $content);
+        }
+        
+        // Replace model-attribute tokens.
+        // Will look for pattern ##model.attribute## and replace the value if found.
+        // Eg ##user.firstname##
+        preg_match_all('/##(.*?)\.(.*?)##/', $content, $matches);
+        
+        if (count($matches) > 0 && count($matches[0]) > 0) {
+            for ($i = 0; $i < count($matches[0]); $i++) {
+                $modelKey = $matches[1][$i];
+                $attributeKey = $matches[2][$i];
+                
+                if (isset($models->$modelKey) && isset($models->$modelKey->$attributeKey)) {
+                    $content = str_replace($matches[0][$i], $models->$modelKey->$attributeKey, $content);
+                }
+            }
+        }
+        
+        // Replace config tokens.
+        $allowedConfigKeys = config('email-templates.config-keys');
+        
+        preg_match_all('/##config\.(.*?)##/', $content, $matches);
+        if (count($matches) > 0 && count($matches[0]) > 0) {
+            for ($i = 0; $i < count($matches[0]); $i++) {
+                $configKey = $matches[1][$i];
+                if (in_array($configKey, $allowedConfigKeys)) {
+                    $configValue = config($configKey);
+                    if ($configValue !== null) {
+                        $content = str_replace($matches[0][$i], $configValue, $content);
+                    }
+                }
+            }
+        }
+        
+        return $content;
     }
 }
