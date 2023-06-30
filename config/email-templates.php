b02908d9554825dcbea873ee59e0216afa7ee26c
Lee Evans <lee@ekouk.com>
2023-06-30 13:55:43 +0100

    Update config to include default languages for dropdown

--- config/email-templates.php
+++ config/email-templates.php
@@ -1,9 +1,18 @@
 <?php
 
 return [
-    'default-locale'=>'en_GB',
-    'default-view'=>'preview-email-template',
-    'header-colour' => '#4a2725',
+    'default-locale' => 'en_GB',
+    'default-view'   => 'preview-email-template',
+    'header-colour'  => '#4a2725',
+    
+    'languages'     => [
+        'en_GB' => ['display' => 'British', 'flag-icon' => 'gb'],
+        'en_US' => ['display' => 'USA', 'flag-icon' => 'us'],
+        'es'    => ['display' => 'Español', 'flag-icon' => 'es'],
+        'fr'    => ['display' => 'Français', 'flag-icon' => 'fr'],
+        'pt'    => ['display' => 'Brasileiro', 'flag-icon' => 'br'],
+        'in'    => ['display' => 'Hindi', 'flag-icon' => 'in'],
+    ],
     
     //Models who can receive emails
     'recipients'    => [
@@ -11,13 +20,13 @@ return [
     ],
     
     //Guards who are authorised to edit email templates
-    'editor-guards'=>['web'],
+    'editor-guards' => ['web'],
     
     /**
      * Allowed config keys which can be inserted into email templates
      * eg use ##config.app.name## in the email template for automatic replacement.
      */
-    'config-keys' => [
+    'config-keys'   => [
         'app.name',
         'app.url',
         // Add other safe config keys here.
