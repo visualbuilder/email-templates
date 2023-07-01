2bc3a002908b0e11735ab650c8f59f1c7461ec28
Lee Evans <lee@ekouk.com>
2023-07-01 21:17:47 +0100

    Standardisation of the template seeder

--- database/seeders/EmailTemplateSeeder.php
+++ database/seeders/EmailTemplateSeeder.php
@@ -20,7 +20,7 @@ class EmailTemplateSeeder extends Seeder
                 'content'   => "<p>Dear ##user.name##,</p>
                                 <p>Thanks for registering with ##config.app.name##.</p>
                                 <p>If you need any assistance please contact our customer services team ##config.email-templates.customer-services## who will be happy to help.</p>
-                                <p>Kindest Regards<br>
+                                <p>Kind Regards<br>
                                 ##config.app.name##</p>"
             ],
             [
@@ -31,14 +31,14 @@ class EmailTemplateSeeder extends Seeder
                 'title'     => 'Reset your password',
                 'subject'   => '##config.app.name## Password Reset',
                 'preheader' => 'Reset Password',
-                'content'   => "<p>Hello ##user.name##,</p><br>
+                'content'   => "<p>Hello ##user.name##,</p>
                                 <p>You are receiving this email because we received a password reset request for your account.</p>
                                 <div>##button url='##tokenURL##' title='Change My Password'##</div>
-                                <p>If you didn't request this password reset, no further action is needed. However if this has happened more than once in a short space of time, please let us know.</p>>
+                                <p>If you didn't request this password reset, no further action is needed. However if this has happened more than once in a short space of time, please let us know.</p>
                                 <p>We'll never ask for your credentials over the phone or by email and you should never share your credentials</p>
                                 <p>If you’re having trouble clicking the 'Change My Password' button, copy and paste the URL below into your web browser:</p>
                                 <p><a href='##tokenURL##'>##tokenURL##</a></p>
-                                <p>Regards,<br>##config.app.name##</p>"
+                                <p>Kind Regards,<br>##config.app.name##</p>"
             ],
             [
                 'key'       => 'user-password-reset-success',
@@ -50,7 +50,7 @@ class EmailTemplateSeeder extends Seeder
                 'preheader' => 'Success',
                 'content'   => "<p>Dear ##user.name##,</p>
                                 <p>Your password has been reset.</p>
-                                <p>Regards,<br>##config.app.name##</p>"
+                                <p>Kind Regards,<br>##config.app.name##</p>"
             ],
             [
                 'key'       => 'user-locked-out',
@@ -62,7 +62,8 @@ class EmailTemplateSeeder extends Seeder
                 'preheader' => 'Oops!',
                 'content'   => "<p>Dear ##user.name##,</p>
                                 <p>Sorry your account has been locked out due to too many bad password attempts.</p>
-                                <p>Please contact our customer services team on ##config.email-templates.customer-services## who will be able to help</p>"
+                                <p>Please contact our customer services team on ##config.email-templates.customer-services## who will be able to help</p>
+                                 <p>Kind Regards,<br>##config.app.name##</p>"
                                 
             ],
             [
@@ -74,8 +75,12 @@ class EmailTemplateSeeder extends Seeder
                 'subject'   => 'Verify your email with ##config.app.name##',
                 'preheader' => 'Gain Access Now',
                 'content'   => "<p>Dear ##user.name##,</p>
-                                <p>Please click the button below to verify your email address.</p>
-                                <div>##button url='##verificationUrl##' title='Verify Email Address'##</div>"
+                                <p>Your receiving this email because your email address has been registered on ##config.app.name##.</p>
+                                <p>To activate your account please click the button below.</p>
+                                <div>##button url='##verificationUrl##' title='Verify Email Address'##</div>
+                                <p>If you’re having trouble clicking the 'Verify Email Address' button, copy and paste the URL below into your web browser:</p>
+                                <p><a href='##verificationUrl##'>##verificationUrl##</a></p>
+                                <p>Kind Regards,<br>##config.app.name##</p>"
             ],
             [
                 'key'       => 'user-verified',
@@ -87,8 +92,7 @@ class EmailTemplateSeeder extends Seeder
                 'preheader' => 'Verification success for ##config.app.name##',
                 'content'   => "<p>Hi ##user.name##,</p>
                                 <p>Your email address ##user.email## has been verified on ##config.app.name##</p>
-                                <p>Kind Regards,<br>
-                                ##config.app.name##</p>"
+                                <p>Kind Regards,<br>##config.app.name##</p>"
             ],
             [
                 'key'       => 'user-login',
@@ -102,8 +106,7 @@ class EmailTemplateSeeder extends Seeder
                                 <p>You have been logged into ##config.app.name##.</p>
                                 <p>If this was not you please contact: </p>
                                 <p>You can disable this email in your account notification preferences.</p>
-                                <p>Kind Regards,<br>
-                                ##config.app.name##</p>"
+                                <p>Kind Regards,<br>##config.app.name##</p>"
             ],
         ];
 
