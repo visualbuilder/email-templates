23a186db2d81480bf3c1b87410f00008d474d1bf
cannycookie <cannycookie@users.noreply.github.com>
2023-09-06 16:02:50 +0000

    Fix styling

--- src/Models/EmailTemplateTheme.php
+++ src/Models/EmailTemplateTheme.php
@@ -2,7 +2,6 @@
 
 namespace Visualbuilder\EmailTemplates\Models;
 
-use Visualbuilder\EmailTemplates\Database\Factories\EmailTemplateFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
